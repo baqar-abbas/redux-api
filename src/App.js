89ce2a42084ea03c05bc417dbd8760f4f299c875
baqar-abbas <baqar007@hotmail.com>
2023-04-14 11:25:10 +0500

    connect React & Redux app to API

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import UsersView from "./components/UsersView";
+
+function App() {
+  return (
+    <div className="App">
+      <UsersView />
+    </div>
+  );
+}
+
+export default App;
